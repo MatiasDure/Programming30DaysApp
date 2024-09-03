24cb844e7d7d0a2c7346661db72ad4cb8cfd1eb6
Matias Dure <497232@student.saxion.nl>
2024-09-03 16:26:18 +0200

    initial commit: images and string resources

---
+++ app/src/main/java/com/example/a30dayapp/ActivityScreen.kt
@@ -0,0 +1,4 @@
+package com.example.a30dayapp
+
+import androidx.compose.runtime.Composable
+import com.example.a30dayapp.model.ProgrammingActivity
---
+++ app/src/main/java/com/example/a30dayapp/model/ProgrammingActivity.kt
@@ -0,0 +1,4 @@
+package com.example.a30dayapp.model
+
+data class ProgrammingActivity(var titleRes: Int, var descriptionRes: Int, var imageRes: Int)
+
